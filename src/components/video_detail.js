3432215dca625d49c3cb5f3b2a01e38570cc8ec4
Albert Chang <albertchanged@gmail.com>
2017-08-08 03:19:30 -0700

    Re-added video-detail.js

---
+++ src/components/video_detail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import VideoList from './video_list';
+
+const VideoDetail = ({video, videoId}) => {
+  if (!video) {
+    return <div>Loading...</div>
+  }
+
+  videoId = video.id.videoId;
+  console.log(videoId);
+  const url = "https://www.youtube.com/embed/" + videoId + "?vq=hd1080?rel=0;&autoplay=1&loop=1&playlist=" + videoId;
+  const externalUrl = "https://www.youtube.com/watch?v=" + videoId;
+  // console.log(url);
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+          <div><a href={externalUrl} id="link" target="_blank">{video.snippet.title}</a></div>
+          <div>{video.snippet.channelTitle}</div>
+        <br></br>
+        <div className="description">{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
