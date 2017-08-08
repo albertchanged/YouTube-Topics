2a4b9d56d63961b5f1a9efa950fd6060e6f98e2b
Albert Chang <albertchanged@gmail.com>
2017-08-08 01:38:11 -0700

    Update video_detail.js

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -18,12 +18,8 @@ const VideoDetail = ({video, videoId}) => {
       </div>
       <div className="details">
         <strong>
-          <div>
-            <a href={externalUrl} id="link" target="_blank">{video.snippet.title}</a>
-          </div>
-          <div>
-            {video.snippet.channelTitle}
-          </div>
+          <div><a href={externalUrl} id="link" target="_blank">{video.snippet.title}</a></div>
+          <div>{video.snippet.channelTitle}</div>
         </strong>
         <br></br>
         <div className="description">{video.snippet.description}</div>
